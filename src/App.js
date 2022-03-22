875e28137009df6d489cce3d4a44fcc53ead6585
Karsten <karsten.jepsen@daugherty.com>
2022-03-21 21:40:14 -0500

    basic functionality for crazy quote and lotr quote working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,211 @@
-import logo from './logo.svg';
+import ye from './ye.png';
+import ring from './ring.png';
+import fotr from './fotr.jpg';
 import './App.css';
+import { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+import Theater from '@material-ui/icons/Theaters';
+import Mic from '@material-ui/icons/MicNone';
+import Check from '@material-ui/icons/CheckCircle';
+import Ex from '@material-ui/icons/HighlightOff';
+import { green, red } from '@material-ui/core/colors';
+import { emojisplosion } from "emojisplosion";
+import gollum from './gollum.mp3'
 
 function App() {
+
+  const [yeQuote, setYeQuote] = useState(null);
+  const [trumpQuote, setTrumpQuote] = useState(null);
+  const [showCrazy, setShowCrazy] = useState(null);
+  const rndmLotrQuote = require('random-lotr-movie-quote');
+  const [lotrQuote, setLotrQuote] = useState(null);
+  const [showMovie, setShowMovie] = useState(null);
+  
+  const fetchLotr = () => {
+    setLotrQuote([rndmLotrQuote()]);
+    setYeQuote(null);
+    setTrumpQuote(null);
+    setShowMovie(null);
+    console.log(rndmLotrQuote());
+  }
+
+  const fetchCrazy = async () => {
+    setLotrQuote(null)
+    setShowCrazy(null);
+    {Math.random() > .5 ?
+    fetchYe()
+    :
+    fetchTrump()
+    }
+  }
+
+  const fetchYe = async () => {
+    setTrumpQuote(null)
+    await fetch("https://api.kanye.rest")
+      .then((response) => response.json())
+      .then((data) => setYeQuote([data]))
+      .catch(() => console.log('error'))
+  }
+
+  const fetchTrump = async () => {
+    setYeQuote(null)
+    await fetch("https://api.whatdoestrumpthink.com/api/v1/quotes")
+      .then((response) => response.json())
+      .then((data) => setTrumpQuote([data]))
+      .catch(() => console.log('error'))
+  }
+
+  const crazyAnswer = (answer) => {
+    if (answer === 'ye'){
+      setShowCrazy('winner')
+      emojisplosion({
+        emojiCount: 201,
+        emojis: ["🪓", "🏹", "🗡️", "⚔️", "🛡️", "👑"],
+      });
+    } else if (answer === 'trump'){
+      setShowCrazy('winner')
+      emojisplosion({
+        emojiCount: 201,
+        emojis: ["🪓", "🏹", "🗡️", "⚔️", "🛡️", "👑"],
+      });
+    } else if (answer === 'loser') {
+      setShowCrazy('loser');
+    }
+  }
+
+  const movieAnswer = (answer) => {
+    if (answer == lotrQuote[0].movie) {
+      setShowMovie('winner');
+      emojisplosion({
+        emojiCount: 201,
+        emojis: ["🪓", "🏹", "🗡️", "⚔️", "🛡️", "👑"],
+      });
+      emojisplosion({
+        emojiCount: 201,
+        emojis: ["🪓", "🏹", "🗡️", "⚔️", "🛡️", "👑"],
+      });
+      emojisplosion({
+        emojiCount: 201,
+        emojis: ["🪓", "🏹", "🗡️", "⚔️", "🛡️", "👑"],
+      });
+    } else {
+      setShowMovie('loser');
+      gollum.play();
+    }
+  }
+
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>What quote would you like today?</h1>
       </header>
+
+      <div className='container'>
+        <Stack direction="row" spacing={16} justifyContent="center" divider={<Divider orientation="vertical" flexItem color="white"/>}>
+          <Stack direction="column" spacing={4} alignItems="center">
+            <img src={ye} className="App-logo" alt="ye" />
+            <Button style={{borderRadius: 45}} variant="outlined" size="large" color="error" endIcon={<Mic />} onClick={fetchCrazy}>Crazy Quote</Button>
+          </Stack>
+          <Stack direction="column" spacing={4} alignItems="center">
+            <img src={ring} className="App-logo" alt="ring" />
+            <Button style={{borderRadius: 45}} variant="outlined" size="large" color="warning" endIcon={<Theater />} onClick={fetchLotr}> Lotr Quote</Button>
+          </Stack>
+        </Stack>
+      </div>
+  
+      { yeQuote?.map(quote => 
+        <div className='quote'>
+          <div className='yeQuote'>
+          <p>"{quote.quote}"</p>
+          </div>
+          { showCrazy === null &&
+          <>
+            <p>Which crazy has this quote?</p>
+            <Stack  direction="row" spacing={3} justifyContent="center">
+              <Button style={{borderRadius: 45}} variant="outlined" size="large" color="success" onClick={() => crazyAnswer('ye')}>Ye</Button>
+              <Button style={{borderRadius: 45}} variant="outlined" size="large" color="error" onClick={() => crazyAnswer('loser')}>Donald Trump</Button>
+            </Stack>
+          </>
+          }
+          { showCrazy === 'winner' &&
+             <>
+             <p>Ye</p>
+             <Check size="large" style={{ color: green[500], fontSize: 50 }}/>
+             </>
+          }
+          { showCrazy === 'loser' &&
+          <>
+            <p>Ye</p>
+            <Ex size="large" style={{ color: red[500], fontSize: 50 }}/>
+          </>
+          }
+        </div>
+      )}
+
+      { trumpQuote?.map(quote => 
+        <div className='quote'>
+          <div className='yeQuote'>
+          <p>"{quote.messages.non_personalized[Math.floor(Math.random() * 49)]}"</p>
+          </div>
+          { showCrazy === null &&
+          <>
+          <p>Which crazy has this quote?</p>
+          <Stack  direction="row" spacing={3} justifyContent="center">
+            <Button style={{borderRadius: 45}} variant="outlined" size="large" color="success" onClick={() => crazyAnswer('loser')}>Ye</Button>
+            <Button style={{borderRadius: 45}} variant="outlined" size="large" color="error" onClick={() => crazyAnswer('trump')}>Donald Trump</Button>
+          </Stack>
+          </>
+          }
+          { showCrazy === 'winner' &&
+             <>
+             <p>Donald Trump</p>
+             <Check size="large" style={{ color: green[500], fontSize: 50 }}/>
+             </>
+          }
+           { showCrazy === 'loser' &&
+          <>
+            <p>Donald Trump</p>
+            <Ex size="large" style={{ color: red[500], fontSize: 50 }}/>
+          </>
+          }
+        </div>
+      )}
+        
+      { lotrQuote?.map(quote => 
+        <div className='quote'>
+          <div className='lotrQuote'>
+            <p>"{quote.dialog}"</p>
+            <p>- {quote.char}</p>
+          </div>
+          {/* <img src={fotr} className="movie" alt="fotr" onClick={() => setShowMovie(true)}/> */}
+          { showMovie === null &&
+            <>
+            <p>Which film has this quote?</p>
+            <Stack  direction="row" spacing={3} justifyContent="center">
+              <Button style={{borderRadius: 45}} variant="outlined" size="large" color="success" onClick={() => movieAnswer('The Fellowship of the Ring ')}>Fellowship of the Ring</Button>
+              <Button style={{borderRadius: 45}} variant="outlined" size="large" color="error" onClick={() => movieAnswer('The Two Towers ')}>The Two Towers</Button>
+              <Button style={{borderRadius: 45}} variant="outlined" size="large" color="primary" onClick={() => movieAnswer('The Return of the King ')}>Return of the King</Button>
+            </Stack>
+            </>
+          }
+          { showMovie === 'winner' &&
+            <>
+            <p>{quote.movie}</p>
+            <Check size="large" style={{ color: green[500], fontSize: 50 }}/>
+            </>
+          }
+          { showMovie === 'loser' &&
+          <>
+            <p>{quote.movie}</p>
+            <Ex size="large" style={{ color: red[500], fontSize: 50 }}/>
+          </>
+          }
+        </div>
+      )}
+
     </div>
   );
 }
